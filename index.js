68e1a6ec29874b78cba2bb47f5741ac01e6a698c
bengroves <bengroves15@gmail.com>
2021-01-25 17:43:09 -0500

    commit tree d76542de40f2c328a16e8802eab4ff3a59d9af3a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// let element = document.createElement('div');
+// document.body.appendChild(element);
+// let ul = document.createElement('ul');
+// explorer.exe index.html
+// let header_one =  document.createElement('h1#victory');
+// document.body.appendChild(header_one);
+let newHeader = document.getElementById('victory');
+newHeader.innerHTML = "is the champion";
+
+
